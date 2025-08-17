c4a6d6d47a84d3c56a5d5fe52d3df2059e6cd7b7
vparikh <vparikh35@gatech.edu>
2025-08-17 10:19:22 -0400

    added static assertions for size guarantees in integer.h

--- RocketLib/FatFs/src/integer.h
+++ RocketLib/FatFs/src/integer.h
@@ -17,21 +17,26 @@ typedef unsigned __int64 QWORD;
 /* These types MUST be 16-bit or 32-bit */
 typedef int				INT;
 typedef unsigned int	UINT;
+_Static_assert((sizeof(INT) == 2 || sizeof(INT) == 4) && (sizeof(UINT) == 2 || sizeof(UINT) == 4));
 
 /* This type MUST be 8-bit */
 typedef unsigned char	BYTE;
+_Static_assert(sizeof(BYTE) == 1);
 
 /* These types MUST be 16-bit */
 typedef short			SHORT;
 typedef unsigned short	WORD;
 typedef unsigned short	WCHAR;
+_Static_assert(sizeof(SHORT) == 2 && sizeof(WORD) == 2 && sizeof(WCHAR) == 2);
 
 /* These types MUST be 32-bit */
 typedef long			LONG;
 typedef unsigned long	DWORD;
+_Static_assert(sizeof(LONG) == 4 && sizeof(DWORD) == 4);
 
 /* This type MUST be 64-bit (Remove this for ANSI C (C89) compatibility) */
 typedef unsigned long long QWORD;
+_Static_assert(sizeof(QWORD) == 8);
 
 #endif
 

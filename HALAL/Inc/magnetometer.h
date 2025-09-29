0030f2e3479c05da3191d1c95a4fc3265fb2fa0f
Renato Dell'Osso <rddellosso4@gmail.com>
2025-09-29 18:54:24 -0400

    Fix a few things

--- HALAL/Inc/magnetometer.h
+++ HALAL/Inc/magnetometer.h
@@ -1,30 +1,16 @@
-/**
-  ******************************************************************************
-  * @file    LIS3MDL.h
-  * @author  Kanav Chugh
-  * @brief   LIS3MDL magnetometer code
-  *
-  *
-  *
-  ******************************************************************************
-  */
-
-#ifndef __LIS3MDL_H__
-#define __LIS3MDL_H__
-#include "stm32h7xx_hal.h"
+#ifndef MAGNETOMETER_H
+#define MAGNETOMETER_H
 #include <stdint.h>
 
-enum magnetometer_err {
+typedef enum {
     MAG_ERR_OK,
     MAG_ERR_GENERAL,
-};
+} magnetometer_err;
 
-enum magnetometer_err HALAL_magnetometer_initialize();
-enum magnetometer_err HALAL_magnetometer_write_hard_iron(double *hard_iron_offset);
-enum magnetometer_err HALAL_magnetometer_read_hard_iron(double *hard_iron_offset);
-enum magnetometer_err HALAL_magnetometer_sensitivity_get(double *sensitivity);
-enum magnetometer_err HALAL_magnetometer_read_temp(double *temp);
+magnetometer_err HALAL_magnetometer_initialize();
+magnetometer_err HALAL_magnetometer_write_hard_iron(double *hard_iron_offset);
+magnetometer_err HALAL_magnetometer_read_hard_iron(double *hard_iron_offset);
+magnetometer_err HALAL_magnetometer_read_temp(double *temp);
+magnetometer_err HALAL_magnetometer_read_mag(double *mag_reading);
 
-enum magnetometer_err HALAL_magnetometer_read_mag(double *mag_reading);
-
-#endif /* __LIS3MDL_H__ */
+#endif

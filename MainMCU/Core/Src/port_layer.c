f22f39196d8c5fc3736f1a92c08662a780de9ff0
vparikh <vparikh35@gatech.edu>
2026-04-07 20:05:24 -0400

    successfully read state from MainMCU

--- MainMCU/Core/Src/port_layer.c
+++ MainMCU/Core/Src/port_layer.c
@@ -25,11 +25,32 @@ uint8_t telemetry_rx_sb_storage[TELEMETRY_RX_MAX_PROCESS_SIZE + 2];
 StaticStreamBuffer_t telemetry_rx_sb_buff;
 
 StreamBufferHandle_t g_state_rx_sb_handle;
-uint8_t state_rx_sb_storage[TELEMETRY_RX_MAX_PROCESS_SIZE + 2];
+uint8_t state_rx_sb_storage[64];
 StaticStreamBuffer_t state_rx_sb_buff;
 
 /* Global rocket state struct */
 RocketState g_current_state = {0};
+
+uint8_t state_uart_rx_buf[64];
+
+void master_task(void* args) {
+    HAL_UART_Transmit(&debug_uart, "Started master task!\r\n", 22, HAL_MAX_DELAY);
+
+    while (1) {
+        uint8_t state_rx_buffer[16];
+        xStreamBufferReceive(g_state_rx_sb_handle, state_rx_buffer, 16, portMAX_DELAY);
+
+        float a, b, c, d;
+        memcpy(&a, state_rx_buffer, sizeof(float));
+        memcpy(&b, state_rx_buffer + 4, sizeof(float));
+        memcpy(&c, state_rx_buffer + 8, sizeof(float));
+        memcpy(&d, state_rx_buffer + 12, sizeof(float));
+
+        char msg_buffer[256];
+        size_t msg_size = sprintf(msg_buffer, "%.4f %.4f %.4f %.4f\r\n", a, b, c, d);
+        HAL_UART_Transmit(&debug_uart, (uint8_t*)msg_buffer, msg_size, HAL_MAX_DELAY);
+    }
+}
 
 /**
  * @brief Initialize the application
@@ -61,13 +82,48 @@ int port_init(void) {
     g_run_controls_task_handle = xTaskCreateStatic(run_controls_task, "run_controls_task", 4096, NULL, tskIDLE_PRIORITY, run_controls_task_stack, &run_controls_task_buff);
     if (g_run_controls_task_handle == NULL) return 0;
     
-    /* Begin listening over uart /
-    if (!begin_uart_listen()) {
+    */
+
+    g_run_controls_task_handle = xTaskCreateStatic(
+        master_task, 
+        "run_controls_task", 
+        4096, 
+        NULL, 
+        1, 
+        run_controls_task_stack, 
+        &run_controls_task_buff
+    );
+    if (g_run_controls_task_handle == NULL) return 0;
+
+    g_state_rx_sb_handle = xStreamBufferCreateStatic(
+        64, 1, state_rx_sb_storage, &state_rx_sb_buff);
+    if (g_state_rx_sb_handle == NULL) return 0;
+
+    if (HAL_UARTEx_ReceiveToIdle_IT(&state_uart, state_uart_rx_buf, 16) != HAL_OK) {
         return 0;
     }
-    */
+
+    HAL_UART_Transmit(&state_uart, "r", 1, HAL_MAX_DELAY);
     
     return 1;
+}
+
+void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t size) {
+    /* FreeRTOS boilerplate */
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+
+    /* Direct uart data to the appropriate place */
+    if (huart->Instance == telemetry_uart.Instance) {
+        /* Send telemetry data to the telemetry_rx task */
+        //xStreamBufferSendFromISR(g_telemetry_rx_sb_handle, telemetry_uart_rx_buf, size, &xHigherPriorityTaskWoken);
+        //HAL_UARTEx_ReceiveToIdle_IT(&telemetry_uart, telemetry_uart_rx_buf, TELEMETRY_RX_MAX_PROCESS_SIZE);
+    } else if (huart->Instance == state_uart.Instance) {
+        xStreamBufferSendFromISR(g_state_rx_sb_handle, state_uart_rx_buf, size, &xHigherPriorityTaskWoken);
+        HAL_UARTEx_ReceiveToIdle_IT(&state_uart, state_uart_rx_buf, STATE_PACKET_SIZE);
+    }
+
+    /* FreeRTOS boilerplate */
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
 }
 
 /**
@@ -75,13 +131,11 @@ int port_init(void) {
  */
 void port_start(void) {
 
-    HAL_TIM_PWM_Start(PWM0_TIMER, PWM0_CHANNEL);
+    //HAL_TIM_PWM_Start(PWM0_TIMER, PWM0_CHANNEL);
 
-    uint16_t count = 1000.0 * 3.2;
-    __HAL_TIM_SET_COMPARE(PWM0_TIMER, PWM0_CHANNEL, count);
+    //uint16_t count = 1000.0 * 3.2;
+    //__HAL_TIM_SET_COMPARE(PWM0_TIMER, PWM0_CHANNEL, count);
 
-    while (1) { 
-
-    }
-    //vTaskStartScheduler();
+    
+    vTaskStartScheduler();
 }
--- MainMCU/Core/Src/uart_isr.c
+++ MainMCU/Core/Src/uart_isr.c
@@ -1,7 +1,7 @@
 #include "uart_isr.h"
 
 uint8_t telemetry_uart_rx_buf[TELEMETRY_RX_MAX_PROCESS_SIZE];
-uint8_t state_uart_rx_buf[STATE_PACKET_SIZE];
+// uint8_t state_uart_rx_buf[STATE_PACKET_SIZE];
 
 uint8_t state_bytes[STATE_PACKET_SIZE];
 
@@ -23,21 +23,22 @@ int begin_uart_listen(void) {
         return 0;
     }
     
-    if (HAL_UARTEx_ReceiveToIdle_IT(&state_uart, state_uart_rx_buf, STATE_PACKET_SIZE) != HAL_OK) {
-        return 0;
-    }
+    //if (HAL_UARTEx_ReceiveToIdle_IT(&state_uart, state_uart_rx_buf, STATE_PACKET_SIZE) != HAL_OK) {
+    //    return 0;
+    //}
 
     return 1;
 }
 
 // #ifndef STATIC_FIRE
+/*
 void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t size) {
-    /* FreeRTOS boilerplate */
+    /* FreeRTOS boilerplate //
     BaseType_t xHigherPriorityTaskWoken = pdFALSE;
 
-    /* Direct uart data to the appropriate place */
+    /* Direct uart data to the appropriate place /
     if (huart->Instance == telemetry_uart.Instance) {
-        /* Send telemetry data to the telemetry_rx task */
+        /* Send telemetry data to the telemetry_rx task /
         xStreamBufferSendFromISR(g_telemetry_rx_sb_handle, telemetry_uart_rx_buf, size, &xHigherPriorityTaskWoken);
         HAL_UARTEx_ReceiveToIdle_IT(&telemetry_uart, telemetry_uart_rx_buf, TELEMETRY_RX_MAX_PROCESS_SIZE);
     } else if (huart->Instance == state_uart.Instance) {
@@ -45,6 +46,7 @@ void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t size) {
         HAL_UARTEx_ReceiveToIdle_IT(&state_uart, state_uart_rx_buf, STATE_PACKET_SIZE);
     }
 
-    /* FreeRTOS boilerplate */
+    /* FreeRTOS boilerplate /
     portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
 }
+*/

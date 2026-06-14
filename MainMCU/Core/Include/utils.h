5c6fdfe12eaa47d5749efc9f7992e5e4775ba843
vparikh <vparikh35@gatech.edu>
2026-06-14 12:07:20 -0700

    purged all files

--- MainMCU/Core/Include/utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-void log_debug_message(char* fmt_str, ...);
-
-uint32_t wait_for_notification(uint32_t notification_mask);
-
-#endif
-
--- MainMCU/Core/Tests/Include/uart_test.h
+++
@@ -1,6 +0,0 @@
-#ifndef UART_TEST_H
-#define UART_TEST_H
-
-void uart_test_task(void *args);
-
-#endif

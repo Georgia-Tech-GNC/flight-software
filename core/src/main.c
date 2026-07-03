7ce0cabcd2b058ff25b1217c6fbba6ce88a2e4ac
vparikh <vparikh35@gatech.edu>
2026-07-03 15:33:36 -0700

    added doxygen

--- core/src/main.c
+++ core/src/main.c
@@ -1,7 +1,15 @@
+/** @file main.c
+ * @brief General entry point for all targets
+ */
+
 #include "main.h"
 #include "port.h"
 
 
+
+/** 
+ * The main methods of all targets are expected to call this method once they are fully initialized
+ */
 void shared_main(void) {
     HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_SET);
 

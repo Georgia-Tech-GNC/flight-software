74cc35c3f46b50b2ee9f9a0217e94612c3f5df06
vparikh <vparikh35@gatech.edu>
2026-07-04 13:57:57 -0700

    test for cppcheck failure

--- core/src/main.c
+++ core/src/main.c
@@ -5,21 +5,23 @@
 #include "main.h"
 #include "port.h"
 
+void example(int x);
 
+void example(int x) {
+    if (x > 0) {
+        __asm__ volatile("nop");
+    } else if (x > 0) {  // cppcheck: "Expression is always false"
+        __asm__ volatile("nop");
+    }
+}
 
 /** 
  * The main methods of all targets are expected to call this method once they are fully initialized
  */
 void shared_main(void) {
     HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_SET);
-    int x;
-    int y;
 
-    for (int i = 0; i < 10; i++) {
-        int y = 5;
-        y++;
-        
-    }
+    example(5);
 
     while (true) {
         HAL_Delay(500);

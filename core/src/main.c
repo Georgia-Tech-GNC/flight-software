d997c637817e3199c00cce95ebe3f8c33e10554b
vparikh <vparikh35@gatech.edu>
2026-07-04 15:19:03 -0700

    update cppcheck

--- core/src/main.c
+++ core/src/main.c
@@ -4,23 +4,11 @@
 
 #include "main.h"
 
-void example(int x);
-
-void example(int x) {
-    if (x > 0) {
-        __asm__ volatile("nop");
-    } else if (x > 0) {  // cppcheck: "Expression is always false"
-        __asm__ volatile("nop");
-    }
-}
-
 /** 
  * The main methods of all targets are expected to call this method once they are fully initialized
  */
 void shared_main(void) {
     HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_SET);
-
-    example(5);
 
     while (true) {
         HAL_Delay(500);

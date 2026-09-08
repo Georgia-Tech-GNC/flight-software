1f2b846c7a13a79b361fae6c8e8bbaee8934b59a
Sam Peterson <sam.peterson1@icloud.com>
2026-09-07 23:33:31 -0500

    fix

--- .github/scripts/renode_test.sh
+++ .github/scripts/renode_test.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 
+python -m venv renode/tests/.venv
+source renode/tests/.venv/bin/activate
+
 curl -O https://builds.renode.io/renode-1.16.1+20260904git63d4e2dd5.linux-portable.tar.gz
 tar -xzf renode-1.16.1+20260904git63d4e2dd5.linux-portable.tar.gz
+pip install -r renode_1.16.1+20260904git63d4e2dd5-portable/tests/requirements.txt
+
 
 renode_1.16.1+20260904git63d4e2dd5-portable/renode-test renode/tests/test.robot \
     --variable VARIABLE_FILE:targets/Nucleo-h723zg/robot_config.yaml \

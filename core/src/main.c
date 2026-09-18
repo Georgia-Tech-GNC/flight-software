fa6d1574a4014818cc09e1a62aec2a79aed7cb71
Nathan Q <Natethegreat08q@gmail.com>
2026-09-18 13:50:44 -0400

    Part 3 - Nathan Q

--- core/src/main.c
+++ core/src/main.c
@@ -36,11 +36,83 @@
  *      - HAL_DELAY(<DELAY IN MS>)
  */
 
+// lsm6dso task data
+static constexpr uint32_t lsm6dso_task_stack_size = 4096 / sizeof(StackType_t);
+static StackType_t lsm6dso_task_stack[lsm6dso_task_stack_size];
+static StaticTask_t lsm6dso_task_buffer;
+static TaskHandle_t lsm6dso_task_handle;
+
+// led task data
+static constexpr uint32_t led_task_stack_size = 4096 / sizeof(StackType_t);
+static StackType_t led_task_stack[led_task_stack_size];
+static StaticTask_t led_task_buffer;
+static TaskHandle_t led_task_handle;
+
+static TaskHandle_t uart_notify_handle = NULL;
+
+// uart recv data
+static char uart_recv_char = '\0';
+
+// Utility function
 static void uart_send_message(const char* str) {
-    uint16_t length = (uint16_t)strlen(str);
-    uint16_t size = sizeof(char) * length;
+    const uint16_t length = (uint16_t)strlen(str);
+    const uint16_t size = sizeof(char) * length;
     
     HAL_UART_Transmit(&huart3, (const uint8_t*)str, size, HAL_MAX_DELAY);
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef* huart) {
+    if (huart != &huart3) 
+        return;
+    
+    if (uart_recv_char != '\n' && uart_recv_char != '\r') {
+        HAL_UART_Receive_IT(&huart3, (uint8_t*)&uart_recv_char, 1);
+        
+        return;
+    }
+    
+    if (uart_notify_handle == NULL)
+        return;
+    
+    BaseType_t higher_priority_task_woken = pdFALSE;
+    xTaskNotifyFromISR(uart_notify_handle, 0, eNoAction, &higher_priority_task_woken);
+}
+
+static void lsm6dso_task(void *pvParameters) {
+    UNUSED(pvParameters);
+    
+    char buf[100];
+
+    while (true) {
+        uart_notify_handle = xTaskGetCurrentTaskHandle();
+        HAL_UART_Receive_IT(&huart3, (uint8_t*)&uart_recv_char, 1);
+        xTaskNotifyWait(0, 0, NULL, portMAX_DELAY);
+        
+        const float pitch_rate = lsm6dso_get_pitch_rate(&hspi1);
+        const float roll_rate = lsm6dso_get_roll_rate(&hspi1);
+        const float yaw_rate = lsm6dso_get_yaw_rate(&hspi1);
+        
+        snprintf(
+            buf, 
+            sizeof(buf), 
+            "Roll Rate (dps): %d, Roll Rate (dps): %d, Yaw Rate (dps): %d", 
+            (int)pitch_rate, 
+            (int)roll_rate, 
+            (int)yaw_rate
+        );
+        uart_send_message(buf);
+    }
+}
+
+static void led_task(void* pvParameters) {
+    UNUSED(pvParameters);
+    
+    while (true) {
+        HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_SET);
+        vTaskDelay(pdMS_TO_TICKS(125));
+        HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_RESET);
+        vTaskDelay(pdMS_TO_TICKS(125));
+    }
 }
 
  /** 
@@ -59,24 +131,25 @@ static void uart_send_message(const char* str) {
         uart_send_message("Failed to initialize lsm6dso device!\n");
     }
     
-    {
-        char buf[100];
-        snprintf(buf, sizeof(buf), "Roll Rate (dps): %d, Roll Rate (dps): %d, Yaw Rate (dps): %d", 0, 0, 0);
-        uart_send_message(buf);
-    }
+    lsm6dso_task_handle = xTaskCreateStatic(
+        lsm6dso_task, 
+        "lsm6dso_task", 
+        lsm6dso_task_stack_size, 
+        NULL, 
+        2, 
+        lsm6dso_task_stack, 
+        &lsm6dso_task_buffer
+    );
     
-    while (true) {
-        HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_SET);
-        
-        float pitch_rate = lsm6dso_get_pitch_rate(&hspi1);
-        float roll_rate = lsm6dso_get_roll_rate(&hspi1);
-        float yaw_rate = lsm6dso_get_yaw_rate(&hspi1);
-        
-        char buf[100];
-        snprintf(buf, sizeof(buf), "\rRoll Rate (dps): %d, Roll Rate (dps): %d, Yaw Rate (dps): %d", (int)pitch_rate, (int)roll_rate, (int)yaw_rate);
-        uart_send_message(buf);
-        
-        HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_RESET);
-        HAL_Delay(1000);
-    }
+    led_task_handle = xTaskCreateStatic(
+        led_task, 
+        "led_task", 
+        led_task_stack_size, 
+        NULL, 
+        1, 
+        led_task_stack, 
+        &led_task_buffer
+    );
+    
+    vTaskStartScheduler();
 } 

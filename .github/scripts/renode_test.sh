c5c13c659ac79790ff7c7745e134d7fb9cfe6922
Sam Peterson <sam.peterson1@icloud.com>
2026-09-24 17:03:19 -0500

    test deploy

--- .github/scripts/renode_test.sh
+++ .github/scripts/renode_test.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 
-renode-test renode/tests/test.robot \
-    --variable VARIABLE_FILE:targets/Nucleo-h723zg/robot_config.yaml \
+rm -rf test-results
+mkdir test-results
+cd test-results
+
+renode-test ../renode/tests/test.robot \
+    --variable VARIABLE_FILE:../targets/Nucleo-h723zg/robot_config.yaml \
     --variable FIRMWARE_ELF:build/ncl-h723-dev/flight-software.elf \
-    --variable PROJECT_ROOT:$PWD \
+    --variable PROJECT_ROOT:$PWD/../
+    
